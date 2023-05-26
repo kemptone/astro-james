e1d8f17c39027ae566ec2b86cafbbdf64b3a5715
justin kempton <justin@talentsky.com>
2023-05-25 21:04:47 -0700

    working version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,31 @@
 import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
+// import { presetWind, presetUno, presetAttributify, presetIcons, presetMini, presetTagify, presetTypography } from 'unocss'
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [UnoCSS({
+    injectReset: true
+    // presets: [
+    //     presetWind(),
+    //     presetUno(),
+    //     presetAttributify(),
+    //     presetIcons(),
+    //     presetMini(),
+    //     presetTagify(),
+    //     presetTypography()
+    // ]
+  }), react()]
+});
+
+// from their sample
+// import { defineConfig } from 'astro/config'
+// import UnoCSS from 'unocss/astro'
+
+// export default defineConfig({
+//   integrations: [
+//     UnoCSS({ injectReset: true }),
+//   ],
+// })
---
+++ uno.config.ts
@@ -0,0 +1,47 @@
+import {
+    defineConfig,
+    presetIcons,
+    presetUno,
+    presetAttributify,
+    transformerDirectives,
+  } from 'unocss'
+  
+  export default defineConfig({
+    shortcuts: [
+      { 'i-logo': 'i-logos-astro w-6em h-6em transform transition-800' },
+    ],
+    transformers: [
+      transformerDirectives(),
+    ],
+    presets: [
+      presetUno(),
+      presetAttributify(),
+      presetIcons({
+        extraProperties: {
+          'display': 'inline-block',
+          'vertical-align': 'middle',
+        },
+      }),
+    ],
+  })
+
+
+  // From their own Docs directory
+
+  /*
+
+  // eslint-disable-next-line no-restricted-imports
+import { defineConfig, presetAttributify, presetIcons, presetUno, transformerDirectives } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetIcons(),
+    presetAttributify(),
+  ],
+  transformers: [
+    transformerDirectives(),
+  ],
+})
+
+*/
