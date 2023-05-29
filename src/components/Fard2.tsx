f0a6b4f1b7b6cd9f453f07b020e9a4242d29f9f6
justin kempton <justin@talentsky.com>
2023-05-28 17:06:20 -0700

    FUCI YUOU

--- src/components/Fard2.tsx
+++ src/components/Fard2.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default () => {
-    return (
-        <div className="bg-amber">
-            Fardo the lop
-        </div>
-    )
-}
+  const [] = useState(0);
+
+  return (
+    <div className="
+        bg-red-500 c-yellow-400
+        hover:(bg-blue-500 c-yellow-100)
+        md:(bg-green-500 c-yellow-700)
+        lg:(bg-green-800 c-yellow-300)
+      ">
+      Fardo the lop
+    </div>
+  );
+};
