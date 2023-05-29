8af2e416727c85f26a3f50a8f877f4d0c871188c
justin kempton <justin@talentsky.com>
2023-05-28 17:06:18 -0700

    working uno, bad formatting

--- uno.config.ts
+++ uno.config.ts
@@ -1,34 +1,35 @@
 import {
-    defineConfig,
-    presetIcons,
-    presetUno,
-    presetAttributify,
-    transformerDirectives,
-  } from 'unocss'
-  
-  export default defineConfig({
-    shortcuts: [
-      { 'i-logo': 'i-logos-astro w-6em h-6em transform transition-800' },
-    ],
-    transformers: [
-      transformerDirectives(),
-    ],
-    presets: [
-      presetUno(),
-      presetAttributify(),
-      presetIcons({
-        extraProperties: {
-          'display': 'inline-block',
-          'vertical-align': 'middle',
-        },
-      }),
-    ],
-  })
+  defineConfig,
+  presetIcons,
+  presetUno,
+  presetAttributify,
+  transformerVariantGroup,
+  transformerDirectives
+} from 'unocss'
 
+export default defineConfig({
+  shortcuts: [
+    { 'i-logo': 'i-logos-astro w-6em h-6em transform transition-800' }
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup()
+  ],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      extraProperties: {
+        display: 'inline-block',
+        'vertical-align': 'middle'
+      }
+    })
+  ]
+})
 
-  // From their own Docs directory
+// From their own Docs directory
 
-  /*
+/*
 
   // eslint-disable-next-line no-restricted-imports
 import { defineConfig, presetAttributify, presetIcons, presetUno, transformerDirectives } from 'unocss'
