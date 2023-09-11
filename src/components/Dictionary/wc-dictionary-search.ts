7c4ce7c0ce9641b113e80c9d63fbc0daaa1d44bd
justin kempton <justin@talentsky.com>
2023-09-10 22:02:23 -0700

    really part of last change

--- src/components/Dictionary/wc-dictionary-search.ts
+++ src/components/Dictionary/wc-dictionary-search.ts
@@ -1,16 +1,28 @@
 import ProtoForm from '../ProtoForm/ProtoForm'
 import '../wc-floating-label-input'
 
-type FormType = {}
+type FormType = {
+  word: string
+  nummber_of_letters: string
+}
 
 class WCDictionarySearch extends HTMLElement {
   connectedCallback() {
+
+    const e_button = this.querySelector('button')
+
     ProtoForm<FormType>({
       e_form: this.querySelector('form'),
-      onSubmit: (formData) => {
-        formData.values
-        debugger
+      onIsValid: (formData) => {
+        e_button?.removeAttribute('disabled')
+      },
+      onIsInvalid: (formData) => {
+        e_button?.setAttribute('disabled', 'true')
       }
+      // onSubmit: (formData) => {
+      //   formData.values
+      //   debugger
+      // }
     })
   }
 
