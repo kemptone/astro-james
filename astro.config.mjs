bcb87bebfbab0acede9bd012e20d887c51b2c0ec
justin kempton <justin@talentsky.com>
2023-09-22 09:55:19 -0700

    adding alerts for James

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro';
+// import UnoCSS from 'unocss/astro';
 // import Deno from '@astrojs/deno'
 // import { presetWind, presetUno, presetAttributify, presetIcons, presetMini, presetTagify, presetTypography } from 'unocss'
 // import react from "@astrojs/react";
@@ -9,18 +9,10 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   // adapters: [Deno()],
-  integrations: [UnoCSS({
-    injectReset: true
-    // presets: [
-    //     presetWind(),
-    //     presetUno(),
-    //     presetAttributify(),
-    //     presetIcons(),
-    //     presetMini(),
-    //     presetTagify(),
-    //     presetTypography()
-    // ]
-  }), 
+  integrations: [
+  //   UnoCSS({
+  //   injectReset: true
+  // }), 
   // react(), 
   preact(),
 ]
